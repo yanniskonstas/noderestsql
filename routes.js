d0fff2cae4fd1c83ef88cb9bf22b681ab28b0d72
yanniskonstas <konstas@protonmail.com>
2020-05-13 19:05:46 +0100

    first commit

---
+++ routes.js
@@ -0,0 +1,8 @@
+const employees = require('./employeeController');
+const departments = require('./departmentController');
+
+
+module.exports = {
+    employees,
+    departments
+};
---
+++ settings.js
@@ -0,0 +1,18 @@
+const APIServerPort = 3099;
+const database = {
+    host: 'localhost',
+    post: 3306,
+    user: 'root',
+    password: '',
+    database: 'project'
+};
+
+module.exports = {
+    database,
+    APIServerPort
+}
+
+/*
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+flush privileges;
+*/
